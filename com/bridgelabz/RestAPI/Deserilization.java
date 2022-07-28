e912ae2f30a59bb4678cfc6bb3891bbef2591960
smrutikapatil <smruti.patil17@gmail.com>
2022-07-28 23:33:09 +0530

    first commit

--- com/bridgelabz/RestAPI/Deserilization.java
+++ com/bridgelabz/RestAPI/Deserilization.java
@@ -1,8 +1,12 @@
 package com.bridgelabz.RestAPI;
 
+import java.lang.reflect.Type;
+import java.util.List;
+
 import org.testng.annotations.Test;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 
 import io.restassured.RestAssured;
 
@@ -16,8 +20,20 @@ public class Deserilization {
 
 		String urlString = "http://localhost:3000/posts/17";
 
-		BlogPosts blogPosts17 = RestAssured.given().get(urlString).as(BlogPosts.class);
-		System.out.println("ssp17:" + blogPosts17.toString());
+		BlogPosts blogPosts17 = RestAssured.given()
+											.get(urlString)
+											.as(BlogPosts.class);
+
+		System.out.println("javaobject:" + blogPosts17);
+
+		String urlString1 = "http://localhost:3000/posts";
+		
+		Type type = new TypeReference<List<BlogPosts>>() {}.getType();
+		
+		List<BlogPosts> blogPostsList = RestAssured.get(urlString1)
+				                                   .as(type);
+
+		System.out.println("javaobject1:" + blogPostsList);
 
 	}
 }
